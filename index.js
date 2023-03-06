95d09c069af70f0856b5b64c5885d036b3456147
Gregory Szaro <gregoryszaro@gmail.com>
2023-03-06 16:45:33 -0500

    Completes Dice Game

---
+++ index.js
@@ -0,0 +1,25 @@
+const randomNumber1 = Math.floor(Math.random()*6) + 1;
+
+const randomDiceImg = "dice" + randomNumber1 + ".png";
+
+const randomImgSrc = "images/" + randomDiceImg;
+
+const img1 = document.querySelectorAll('img')[0];
+
+img1.setAttribute('src', randomImgSrc);
+
+
+
+const randomNumber2 = Math.floor(Math.random()*6) + 1;
+const randomImgSrc2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll('img')[1].setAttribute('src', randomImgSrc2);
+
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector('h1').innerHTML = 'Player 1 Wins!';
+}else if(randomNumber1 < randomNumber2){
+    document.querySelector('h1').innerHTML = 'Player 2 Wins!';
+}else {
+    document.querySelector('h1').innerHTML = 'Draw!';
+}
